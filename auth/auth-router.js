02596b8ad1c8ac13e054f9049f676eb3f2227e22
Blayze <blayzestone@protonmail.com>
2020-06-19 09:06:40 -0700

    setup authentication
    * encrypt password stored on DB when registering
    * create session token on register / login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,57 @@
-const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
+const router = require("express").Router();
+
+const { jwtSecret } = require("../config/constants");
+
+const db = require("../users/users-model");
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const rounds = process.env.HASH_ROUNDS || 6;
+    const hash = bcryptjs.hashSync(password, rounds);
+
+    const user = await db.add({ username, password: hash });
+    const token = createToken(user);
+
+    res.status(201).json({ message: "successfully registered", token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "internal server error occured" });
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await db.findBy({ username });
+
+    if (user && bcryptjs.compareSync(password, user.password)) {
+      const token = createToken(user);
+
+      res.status(201).json({ message: "successfully logged in", token });
+    } else {
+      res.status(401).json({ message: "incorrect credentials" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
+
+function createToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role,
+  };
+  const options = {
+    expiresIn: "8h",
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
